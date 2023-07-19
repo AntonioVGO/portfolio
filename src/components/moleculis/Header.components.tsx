5c916ee8c2885f5dab287fe90bdcc7ae58a64b40
AntonioVGO <139768469+AntonioVGO@users.noreply.github.com>
2023-07-19 12:40:34 +0200

    Varios

--- src/components/moleculis/Header.components.tsx
+++ src/components/moleculis/Header.components.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex flex-row content-between w-full p-5 bg-gray-900 ">
+    <header className="flex flex-row content-between w-full p-2 bg-gray-900 ">
       <button onClick={() => navigate("/")} className="flex flex-row w-6/12 font-bold text-white p-4 space-x-5">
         <img className="w-14 h-14" src="https://i.ibb.co/R6VT33J/nombre.png"></img>
         <p className="py-4">TOÑO VARGAS</p>
@@ -19,9 +19,9 @@ export const Header = () => {
 
       <nav className="flex flex-col w-full py-4">
         <ul className="text-white flex flex-row gap-5 font-bold justify-end items-end pr-3">
-          <li><a href="#">PORTFOLIO</a></li>
-          <li><a href="#">SOBRE MÍ</a></li>
-          <li><a href="#">CONTACTO</a></li>
+          <li><button onClick={() => navigate("/portfolio")}>PORTFOLIO</button></li>
+          <li><button onClick={() => navigate("/about")}>SOBRE MÍ</button></li>
+          <li><button onClick={() => navigate("/contacto")}>CONTACTO</button></li>
           <li className="ml-2 h-5/6"><Button2 text="LOGIN / REGISTRO" onClick={() => navigate("/login")} /></li>
         </ul>
       </nav>
--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,9 +1,12 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import App from "../App"
 import { Login } from "../pages/Login/Login.page"
 import { Registro } from "../pages/Registro/Registro.page"
 import { Weather } from "../pages/Weather/Weather.page"
 import { Error404 } from "../pages/404/Error404.page"
+import { Portfolio } from "../pages/Portfolio/Portfolio.page"
+import { About } from "../pages/About/About.page"
+import { Contacto } from "../pages/Contacto/Contacto.page"
 import { ProtectedRoutes } from "./Protected.routes"
 
 
@@ -13,10 +16,14 @@ export default () => {
             <Route path="/" element={<App />} />
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Registro />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacto" element={<Contacto />} />
             <Route element={<ProtectedRoutes />}>
                 <Route path="/weather" element={<Weather />} />
             </Route>
             <Route path="*" element={<Error404 />} />
+            <Route path={"/portfolio"} element={<Navigate to ={"/"}/>} />
         </Routes>
     )
 }
