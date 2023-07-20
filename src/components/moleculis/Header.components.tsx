efec2eebcce16561aefb895e2251e410664214a9
AntonioVGO <139768469+AntonioVGO@users.noreply.github.com>
2023-07-20 14:42:43 +0200

    varios

--- src/components/moleculis/Header.components.tsx
+++ src/components/moleculis/Header.components.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import nombreImg from "../../assets/img/nombre.png"
 import nombredarkImg from "../../assets/img/nombredark.png"
 import { IconoTiempo } from "./IconoTiempo.components";
+import { useState } from "react"; // import state
 
 //import { Button2 } from "../atom/button/Button2.components";
 //import { IconoTiempo } from "./IconoTiempo.components";
@@ -9,6 +10,12 @@ import { IconoTiempo } from "./IconoTiempo.components";
 export const Header = () => {
 
   const navigate = useNavigate();
+
+  const [hidden, setHidden] = useState(false);
+
+  const toggleHidden = () => {
+    setHidden(!hidden) 
+    };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 pr-5">
@@ -18,15 +25,18 @@ export const Header = () => {
           <img src={nombreImg} className="h-8 mr-3 collapse dark:visible"/>
           <span className="self-center text-DM font-semibold whitespace-nowrap dark:text-white">TOÑO VARGAS</span>
         </button>
+
         <button onClick={() => navigate("/weather")} className="flex flex-row w-fit justify-center items-center">
           <IconoTiempo />
         </button>
-        <button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        
+        <button onClick={toggleHidden} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <svg className="w-5 h-5" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"></path>
           </svg>
         </button>
-        <div className="w-full md:block md:w-auto hidden">
+        
+        <div className={`${hidden ? "block" : "hidden"} w-full md:block md:w-auto`}>
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <button onClick={() => navigate("/portfol")} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">PORTFOLIO</button>
@@ -46,36 +56,3 @@ export const Header = () => {
     </nav>
   )
 }
-
-{/*<header className="flex flex-row content-between w-full h-32 p-2 bg-gray-900 justify-center items-center">
-        <button onClick={() => navigate("/")} className="flex flex-row w-6/12 min-w-[15%] font-bold text-white p-4 space-x-5 justify-start items-center">
-          <img className="w-14" src="https://i.ibb.co/R6VT33J/nombre.png"></img>
-          <p className="py-4 text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg">TOÑO VARGAS</p>
-        </button>
-
-        <button onClick={() => navigate("/weather")} className="flex flex-row w-fit justify-center items-center">
-          <IconoTiempo />
-        </button>
-
-        
-
-        <nav className="flex-row invisible md:visible w-full py-4 justify-end items-center mr-5">
-          <ul className="text-white flex flex-row gap-5 font-bold justify-center items-center pr-3">
-            <li><button className="text-sm sm:text-xs md:text-sm lg:text-base xl:text-lg" onClick={() => navigate("/portfol")}>PORTFOLIO</button></li>
-            <li><button className="text-sm sm:text-xs md:text-sm lg:text-base xl:text-lg" onClick={() => navigate("/about")}>SOBRE MÍ</button></li>
-            <li><button className="text-sm sm:text-xs md:text-sm lg:text-base xl:text-lg" onClick={() => navigate("/contacto")}>CONTACTO</button></li>
-            <li className="text-sm sm:text-xs md:text-sm lg:text-base xl:text-l ml-2 h-5/6"><Button2 text="LOGIN / REGISTRO" onClick={() => navigate("/login")} /></li>
-          </ul>
-        </nav>
-
-        <button data-collapse-toggle="navbar-hamburger" type="button" className="mr-5 md:hidden inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
-
-      </header>
-    
-  )
-}*/}
