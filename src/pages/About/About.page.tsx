9794e4190c3c1adaa93e8e28e2db19c4fcfcd802
AntonioVGO <139768469+AntonioVGO@users.noreply.github.com>
2023-07-24 16:29:35 +0200

    .

--- src/pages/About/About.page.tsx
+++ src/pages/About/About.page.tsx
@@ -9,7 +9,7 @@ export const About = () => {
                         <h2 className="flex justify-center">SOBRE MÍ</h2>
                         <p className="pt-5 text-justify">Estudié arquitectura técnica por cercanía, pero siempre he estado más interesado en las artes
                             gráficas, en las que he ido, y sigo, autoformándome. 
-                            Actualmente he estoy formándome en programación, tanto en backend como en frontend, manejando
+                            Actualmente estoy formándome en programación, tanto en backend como en frontend, manejando
                             varios lenguajes.
                             Poseo gran capacidad de asimilación de conceptos y adaptación a nuevos métodos de trabajo.
                             Apuesto por desarrollar mi trabajo en Extremadura. Posibilidad de home-working para el resto
