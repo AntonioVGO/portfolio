390edb10d6446ee92b9c05c396cd98775ef75fb1
AntonioVGO <139768469+AntonioVGO@users.noreply.github.com>
2023-07-24 16:55:17 +0200

    .

--- src/pages/About/About.page.tsx
+++ src/pages/About/About.page.tsx
@@ -4,17 +4,16 @@ export const About = () => {
     return (
         <>
             <Layout>
-                <div className="flex flex-col w-screen h-screen justify-center items-center bg-gray-800">
-                    <div className="w-96 h-fit bg-white flex flex-col p-10">
-                        <h2 className="flex justify-center">SOBRE MÍ</h2>
+                <div className="flex flex-col w-screen h-screen pt-20 items-center bg-gray-200 dark:bg-gray-800">
+                    <article className="prose w-96 h-fit bg-white flex flex-col p-10 dark:bg-white shadow-xl">
+                        <h2 className="flex justify-center font-bold">SOBRE MÍ</h2>
                         <p className="pt-5 text-justify">Estudié arquitectura técnica por cercanía, pero siempre he estado más interesado en las artes
-                            gráficas, en las que he ido, y sigo, autoformándome. 
-                            Actualmente estoy formándome en programación, tanto en backend como en frontend, manejando
-                            varios lenguajes.
+                            gráficas, en las que he ido, y sigo, autoformándome.
+                            Actualmente estoy formándome en programación, tanto en backend como en frontend, manejando varios lenguajes.
                             Poseo gran capacidad de asimilación de conceptos y adaptación a nuevos métodos de trabajo.
-                            Apuesto por desarrollar mi trabajo en Extremadura. Posibilidad de home-working para el resto
-                            del mundo.</p>
-                    </div>
+                            Apuesto por desarrollar mi trabajo en Extremadura. Posibilidad de home-working para el resto del mundo.
+                        </p>
+                    </article>
                 </div>
             </Layout>
         </>
